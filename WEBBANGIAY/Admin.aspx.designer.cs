85f3d41412fb2fa0c9406a4912e0503e6006c739
vohoangphu095 <vohoangphu095@gmail.com>
2025-06-28 13:29:54 +0700

    Add files via upload

--- WEBBANGIAY/Admin.aspx.designer.cs
+++ WEBBANGIAY/Admin.aspx.designer.cs
@@ -40,5 +40,23 @@ namespace WEBBANGIAY
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Literal litProductMessage;
+
+        /// <summary>
+        /// gvUsers control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvUsers;
+
+        /// <summary>
+        /// litUserMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litUserMessage;
     }
 }
